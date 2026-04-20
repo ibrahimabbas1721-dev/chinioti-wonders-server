8782f0ebb2e204723b7c322ac012f46a5e321e02
ibrahimabbas1721-dev <ibrahimabbas1721@gmail.com>
2026-04-20 10:34:15 -0700

    Update CORS with production URLs

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: '*'
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://chinioti-wonders-client.vercel.app',
+    'https://chinioti-wonders-admin.vercel.app'
+  ],
+  credentials: true
 }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
